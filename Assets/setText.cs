a7371a1d78d6fe3072a1171012338695c3e956de
Ben Johnson <baj24268@gmail.com>
2022-02-23 22:39:28 -0500

    Circuit De Barnaco

---
+++ Assets/setText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setText : MonoBehaviour
+{
+
+    public Text text;
+
+    public void Update()
+    {
+
+        if (PlayerPrefs.GetString("lastTime") != "")
+        {
+
+            text.text = PlayerPrefs.GetString("lastTime");
+
+        }
+
+        else {
+
+            text.text = "no data";
+        
+        }
+
+    }
+
+}
---
+++ Assets/timer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+
+    float timeStarted;
+    float lastLap;
+
+    public Text lastLapText;
+    public Text currentLapText;
+
+    bool timerActive;
+
+    private void Start()
+    {
+
+        lastLapText.text = PlayerPrefs.GetString("lastLap");
+
+    }
+
+    public void timerStart() {
+
+        if (!timerActive)
+        {
+
+            timeStarted = Time.time;
+            timerActive = true;
+
+        }
+
+    }
+
+    public void timerStop() {
+
+        if (timerActive)
+        {
+
+            lastLap = Mathf.Round((Time.time - timeStarted) * 100.0f) * 0.01f;
+            lastLapText.text = lastLap.ToString();
+            PlayerPrefs.SetString("lastLap", lastLap.ToString());
+            timerActive = false;
+
+        }
+    
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (timerActive)
+        {
+
+            currentLapText.text = (Mathf.Round((Time.time - timeStarted) * 100.0f) * 0.01f).ToString();
+
+
+        }
+
+        else {
+
+            currentLapText.text = "not active";
+
+
+        }
+    }
+
+}
